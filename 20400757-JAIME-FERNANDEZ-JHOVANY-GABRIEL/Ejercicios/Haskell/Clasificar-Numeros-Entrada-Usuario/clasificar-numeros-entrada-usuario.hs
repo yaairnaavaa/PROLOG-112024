0301518b5e6ba8676ef0b78385aa2d60945a9619
JhovanyITT <jhgajaimefe@ittepic.edu.mx>
2024-12-04 09:25:15 -0700

    Ejercicios día 02/12/2024 con modificación al ejercicio clasificar números entrada usuario

--- 20400757-JAIME-FERNANDEZ-JHOVANY-GABRIEL/Ejercicios/Haskell/Clasificar-Numeros-Entrada-Usuario/clasificar-numeros-entrada-usuario.hs
+++ 20400757-JAIME-FERNANDEZ-JHOVANY-GABRIEL/Ejercicios/Haskell/Clasificar-Numeros-Entrada-Usuario/clasificar-numeros-entrada-usuario.hs
@@ -1,12 +1,16 @@
-clasificarNumero :: Integer -> String
+import Control.Exception (try, evaluate, SomeException)
+
+clasificarNumero :: Int -> String
 clasificarNumero n
     | n < 0     = "Negativo"
     | n == 0    = "Cero"
     | otherwise = "Positivo"
 
 main :: IO ()
-main = do   
+main = do
     putStrLn "Introduce un número para clasificarlo:"
     input <- getLine
-    let number = read input :: Integer
-    putStrLn (clasificarNumero number)
+    result <- try (evaluate (read input :: Int)) :: IO (Either SomeException Int)
+    case result of
+        Left ex  -> putStrLn $ "Por favor ingrese un número"
+        Right number -> print(clasificarNumero number)

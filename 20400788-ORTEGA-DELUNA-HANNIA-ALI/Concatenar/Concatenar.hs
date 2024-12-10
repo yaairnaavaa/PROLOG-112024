3eb9e1ed5858522bef0256f7b2cc6b66434267bb
HanniaDeLuna <105675373+HanniaDeLuna@users.noreply.github.com>
2024-12-10 09:58:16 -0600

    carpeta completa

---
+++ 20400788-ORTEGA-DELUNA-HANNIA-ALI/Concatenar/Concatenar.hs
@@ -0,0 +1,11 @@
+--Concatenas dos cadenas
+concatenar :: String -> String ->String 
+concatenar str1 str2 = str1 ++ str2
+
+--Funcion principal 
+main :: IO ()
+main = do 
+
+    --Prueba de la funcion concatenar 
+    putStrLn "Concatenado 'Hola' y 'Mundo' :"
+    print (concatenar "Hola" "Mundo")
---
+++ 20400788-ORTEGA-DELUNA-HANNIA-ALI/NumPar/NumPar.py
@@ -0,0 +1,11 @@
+esPar :: Int -> Bool
+esPar n = n mod 2 == 0
+
+main :: IO ()
+main = do
+   putStrLn "Ingresa un número: "
+   input <- getLine
+   let numero = read input :: Int
+   if esPar numero
+      then putStrLn "Es par."
+      else putStrLn "Error fatal."
